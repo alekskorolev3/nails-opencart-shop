e62f6f9458cfcc4c91f9d8a90647effc05716acd
GorbachyovKS <gorbachyovKS@gmail.com>
2024-02-21 17:54:19 +0300

    modal main

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.burger-icon').addEventListener('click', () => {
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle('active')
+})
+
+document.querySelector('.modal-burger-icon').addEventListener('click', () => {
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle('active')
+})
