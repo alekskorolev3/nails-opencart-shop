09e718756308a5491e8c4339bc095fafe57aec8a
alekskorolev3 <alekskorolev3@gmail.com>
2024-03-04 14:39:17 +0300

    Minor changes 7

--- js/app.js
+++ js/app.js
@@ -184,22 +184,6 @@ document.querySelector('.modal-wrapper').addEventListener('click', () => {
     modalWrapper.classList.toggle('activeWrapper')
 })
 
-let reaction = document.querySelector('.reaction')
-let options = {
-  root: reaction,
-  rootMargin: "0px",
-  threshold: 1.0,
-};
-let callback = function (entries, observer) {
-  let button = document.querySelector('.basket-rightside-button')
-  if (entries[0].isIntersecting) {
-    button.classList.remove('move-button')
-  }
-  else {
-    if (entries[0].boundingClientRect.top > 0) button.classList.add('move-button')
-  }
-};
-let observer = new IntersectionObserver(callback);
-observer.observe(reaction)
+
 
 
